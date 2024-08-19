3619b02a3c6aba1bc497fdc7fcb5cd7a69a140d4
Stephen Bradshaw <root@Elliss-MacBook-Pro.local>
2024-08-19 10:39:40 -0400

    exercise incomplete

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,11 @@
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
-}
+   return (
+   
+      <div>
+         <h1 className={classes.choresHeading}>Today's Chores</h1>
+         <p className={classes.choresText}>No content is here!</p>;
+      </div>
+
+)}
